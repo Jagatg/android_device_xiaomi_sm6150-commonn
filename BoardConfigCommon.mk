50822152a4f9634fe47dd938c1f191d1c1e2bdc7
Jayedkhan800 <tanvirgaming859@gmail.com>
2026-06-02 13:39:03 +0000

    sm6150-common: add miui camera and dolby

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -173,3 +173,8 @@ WIFI_HIDL_FEATURE_AWARE := true
 WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
 WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 WPA_SUPPLICANT_VERSION := VER_0_8_X
+
+
+
+# MIUI Camera
+include device/xiaomi/miuicamera-sweet/BoardConfig.mk
